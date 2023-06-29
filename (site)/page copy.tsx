b8a1da17be13c1aa86b6dbcdd6af273ea5d3799b
mvn-hanguyen2-hn <ha.nguyen2@monstar-lab.com>
2023-06-29 14:59:46 +0700

    auth-ui

---
+++ (site)/page copy.tsx
@@ -0,0 +1,5 @@
+import Image from "next/image";
+
+export default function Home() {
+  return <div className="text-sky-500 text-3xl">Hello</div>;
+}
---
+++ app/(site)/components/AuthForm.tsx
@@ -0,0 +1,145 @@
+"use client";
+import AuthSocialButton from "@/components/AuthSocialButton/AuthSocialButton";
+import Button from "@/components/Button/Button";
+import Input from "@/components/Input/Input";
+import { useCallback, useState } from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { BsGithub, BsGoogle } from "react-icons/bs";
+
+const VariantValues = {
+  LOGIN: "LOGIN",
+  REGISTER: "REGISTER",
+};
+const SocialActionsValues = {
+  GITGUB: "github",
+  GOOGLE: "google",
+};
+
+export default function AuthForm() {
+  const [variant, setVariant] = useState<String>(VariantValues.LOGIN);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const toggleVariant = useCallback(() => {
+    if (variant === VariantValues.LOGIN) {
+      setVariant(VariantValues.REGISTER);
+    } else {
+      setVariant(VariantValues.LOGIN);
+    }
+  }, [variant]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+
+    if (variant === VariantValues.REGISTER) {
+      // axios call
+    }
+    if (variant === VariantValues.LOGIN) {
+      // nextauth signin
+    }
+  };
+
+  const socialAction = (action: string) => {
+    setIsLoading(true);
+    // nextauth social sign in
+  };
+  return (
+    <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+      <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
+          {variant === VariantValues.REGISTER && (
+            <Input
+              id="name"
+              label={"Name"}
+              type="text"
+              register={register}
+              errors={errors}
+              disabled={isLoading}
+            />
+          )}
+
+          <Input
+            id="email"
+            label={"Email address"}
+            type="email"
+            register={register}
+            errors={errors}
+            disabled={isLoading}
+          />
+          <Input
+            id="password"
+            label={"Password"}
+            type="password"
+            register={register}
+            errors={errors}
+            disabled={isLoading}
+          />
+
+          <div className="">
+            <Button fullWidth disabled={isLoading} type="submit">
+              {variant === VariantValues.LOGIN
+                ? VariantValues.LOGIN
+                : VariantValues.REGISTER}
+            </Button>
+          </div>
+        </form>
+
+        <div className="mt-6">
+          <div className="relative">
+            <div className="absolute inset-0 flex items-center">
+              <div className="w-full border-t border-gray-300"></div>
+            </div>
+
+            <div className="relative flex justify-center text-sm">
+              <div className="bg-white px-2 text-gray-500">
+                Or continue with
+              </div>
+            </div>
+          </div>
+
+          <div className="mt-6 flex gap-2">
+            <AuthSocialButton
+              icon={BsGithub}
+              onClick={() => socialAction(SocialActionsValues.GITGUB)}
+            />
+            <AuthSocialButton
+              icon={BsGoogle}
+              onClick={() => socialAction(SocialActionsValues.GOOGLE)}
+            />
+          </div>
+
+          <div
+            className="
+            flex
+            gap-2 justify-center
+          text-sm
+          mt-6
+          px-2
+          text-gray-500
+          "
+          >
+            <div>
+              {variant === VariantValues.LOGIN
+                ? "New an account?"
+                : "Already have an account?"}
+            </div>
+            <div className="underline cursor-pointer" onClick={toggleVariant}>
+              {variant === VariantValues.LOGIN ? "Create an account" : "Login"}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
